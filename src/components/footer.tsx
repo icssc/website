f9623fe7fb0ab1e2207cf0594eea00f3351e62b9
Kevin Wu <kevinwu098@gmail.com>
2024-12-26 00:30:10 -0800

    style: max width footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,14 @@ import { BsDiscord, BsFacebook, BsInstagram } from "react-icons/bs";
 
 export function Footer() {
     return (
-        <div className="mt-32 flex h-24 flex-col items-center justify-center gap-y-2 bg-ic-black px-16 text-ic-white lg:flex-row lg:justify-between">
-            <div>&#169; 2024 ICSSC. All Rights Reserved.</div>
-
-            <div className="flex space-x-4">
-                <BsFacebook />
-                <BsDiscord />
-                <BsInstagram />
+        <div className="mt-32 flex h-24 justify-center bg-ic-black px-16 text-ic-white">
+            <div className="max-w-screen-3xl flex h-full w-full flex-col items-center justify-center gap-y-2 lg:flex-row lg:justify-between">
+                <div>&#169; 2024 ICSSC. All Rights Reserved.</div>
+                <div className="flex space-x-4">
+                    <BsFacebook />
+                    <BsDiscord />
+                    <BsInstagram />
+                </div>
             </div>
         </div>
     );
